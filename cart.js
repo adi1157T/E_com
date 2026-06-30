f83fef22cb875515808eff26745fee7083f58cc5
adi1157T <adityatomar743@gmail.com>
2026-06-30 18:34:58 +0530

    cart total calculation with dynamic updates

--- cart.js
+++ cart.js
@@ -2,6 +2,7 @@ document.querySelector('#plus-btn') .addEventListener('click',function(){
     let qty = parseInt(document.querySelector('#qty-number').textContent)
     qty = qty+1
     document.querySelector('#qty-number').textContent=qty
+    updateTotal()
 })
 
 document.querySelector('#minus-btn') .addEventListener('click',function(){
@@ -9,10 +10,18 @@ document.querySelector('#minus-btn') .addEventListener('click',function(){
     if (qty>1){
         qty = qty-1
     }
-    
     document.querySelector('#qty-number').textContent=qty
+    updateTotal()
 })
 
 document.querySelector('#remove-btn') .addEventListener('click',function(){
     document.querySelector('#cart-item').remove()
 })
+
+function updateTotal(){
+    let price = parseInt(document.querySelector('#price').dataset.price)
+    let qty   = parseInt(document.querySelector('#qty-number').textContent)
+    let total = price*qty
+    document.querySelector('#cart-total').textContent = `Total Price: ₹${total}`
+}
+updateTotal()
